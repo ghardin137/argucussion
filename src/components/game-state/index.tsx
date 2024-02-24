639633b33a2d24267e5e5ff4e9a93cd604f12df9
Greg Hardin <greg@virtuallite.com>
2024-02-23 23:05:25 -0800

    Updated styles

--- src/components/game-state/index.tsx
+++ src/components/game-state/index.tsx
@@ -6,7 +6,7 @@ import './game-state.css';
 export function GameState({ time} : GameStateProps) {
     return (
         <div className="app__container__game-state">
-            <div className="app__container__game-state__time">{time}</div>
+            <div className={`app__container__game-state__time ${time < 45 && time > 15 ? "low" : ""}${time < 15 ? "danger" : ""}`}>{time}</div>
             <div className="app__container__game-state__spacer" />
         </div>
     );
